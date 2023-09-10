e9ca58fba4f2704f3d3f20db1b4cbb7e5ceb907c
Natan Feithlicher <feithlicher@gmail.com>
2023-09-10 19:17:53 +0300

    first commit -- app is running on my phone

---
+++ app/_layout.js
@@ -0,0 +1,7 @@
+import { Stack } from 'expo-router';
+
+const Layout = () => {
+    return <Stack />; //21:14
+};
+
+export default Layout;
---
+++ app/index.js
@@ -0,0 +1,15 @@
+// created it with: npx create-expo-app@latest --example with-router
+// then: npm install expo-font axios react-native-dotenv
+// then: npm install -g expo-cli
+// to run: npx expo start --tunnel
+import { View, Text } from 'react-native'; // we can view the 'View' as a div and the 'Text' as a <p> tag
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+};
+
+export default Home;
